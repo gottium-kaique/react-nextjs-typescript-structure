6f35c9f8ff230c9b211127f55e4c98bd4b842761
Kaique Araújo <69827216+gottium-kaique@users.noreply.github.com>
2020-09-10 17:33:12 -0300

    Configurações do Prettier

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: false, //Se você quiser usar ponto e vírgula
+  singleQuote: true, 
+  arrowParens: "avoid",
+  trailingComma: "none",//Mantém vírgula no final
+  endOfLine: true,
+}
